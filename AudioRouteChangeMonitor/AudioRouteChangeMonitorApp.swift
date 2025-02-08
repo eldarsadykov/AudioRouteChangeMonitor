7d3cb7ac8b4ec72c9994acef2c8805a4a952589d
Eldar Sadykov <eldar@mooz.pro>
2025-02-08 10:27:30 +0100

    Initial Commit

---
+++ AudioRouteChangeMonitor/AudioRouteChangeMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRouteChangeMonitorApp.swift
+//  AudioRouteChangeMonitor
+//
+//  Created by Eldar Sadykov on 08.02.25.
+//
+
+import SwiftUI
+
+@main
+struct AudioRouteChangeMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioRouteChangeMonitor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioRouteChangeMonitor
+//
+//  Created by Eldar Sadykov on 08.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
